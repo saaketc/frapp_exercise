d64e7c08f6a98482e053768c18d82667cba5d149
saaketc <saaket123@outlook.com>
2020-02-14 21:17:33 +0530

    minor changes

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -3,16 +3,16 @@ const { isOpen, isCircularDependency } = require('../services/taskRouteServices'
 
 router.get('/', (req, res) => { 
     const requestBody = req.body;
-    
+
     // checks for circular dependency of tasks
     if (isCircularDependency(requestBody))
-        return res.status(400).send({ Error: 'Cyclic dependency found in tasks.' });
+        return res.status(400).send({ Error: 'Cyclic dependency found in tasks. Please check tasks again.' });
     
     // checks task is open or not
     if (isOpen(requestBody))
         return res.status(200).send({ open: true });
     
-    return res.status(400).send({ open: false });
+    return res.status(200).send({ open: false });
 });
 
 
